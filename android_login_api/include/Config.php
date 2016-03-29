9e54ee0e5a8bb0885f2fc5046e3b15f83147445e
azharfauzandz <azharfauzandz@gmail.com>
2016-03-29 11:50:13 +0700

    mengubah regiter API dan login API serta menambahkan profile.php untuk testing register dan ogin sesuai flow yang terbaru

---
+++ android_login_api/include/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "azhar");
+define("DB_PASSWORD", "Azhar@13");
+define("DB_DATABASE", "android_api");
+?>
