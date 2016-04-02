fa76e0d40638873ee0b2f4754b74769ffc15f7eb
Rahadian Yusuf <rahadian.y@gmail.com>
2016-04-02 19:17:29 +0700

    Tambah uri untuk update user

--- app/app/src/main/java/com/surverior/android/app/AppConfig.java
+++ app/app/src/main/java/com/surverior/android/app/AppConfig.java
@@ -1,17 +1,20 @@
 package com.surverior.android.app;
 
 public class AppConfig {
+	// URL
+	public static String URL_ROOT = "https://surverior.pomadgw.xyz"
+
 	// Server user login url
-	public static String URL_LOGIN = "https://surverior.pomadgw.xyz/api/authenticate";
+	public static String URL_LOGIN = URL_ROOT + "/api/authenticate";
 
 	// Server user register url
-	public static String URL_REGISTER = "https://surverior.pomadgw.xyz/api/users/register";
+	public static String URL_REGISTER = URL_ROOT + "/api/users/register";
 
-    public static String URL_RENEW_TOKEN = "https://surverior.pomadgw.xyz/api/token";
+    public static String URL_RENEW_TOKEN = URL_ROOT + "/api/token";
 
-	public static String URL_GET_USER_DATA = "https://surverior.pomadgw.xyz/api/users/current";
+	public static String URL_GET_USER_DATA = URL_ROOT + "/api/users/current";
+
+	public static String URL_UPDATE = URL_ROOT + "/api/users/current/update";
 
 	public static String JWT_SECRET = "yGZw4lv9pDXWcptjEXU9ozHctQe7X5Rv";
-
-	public static String URL_UPDATE = "http://192.168.100.2/android_login_api/profile.php";
 }
