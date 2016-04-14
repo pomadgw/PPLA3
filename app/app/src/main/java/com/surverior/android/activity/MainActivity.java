0ad57c8f0eaeef4eed25cc3766b093109dbe41e7
bmgprakoso <bmgprakoso@gmail.com>
2016-04-14 09:22:27 +0700

    Menambahkan toolbar ke dalam activity_main
    - Mengubah styles.xml sesuia dengan material design
    - Menambah dimens
    - Menambah resource baru yaitu menu

--- app/app/src/main/java/com/surverior/android/activity/MainActivity.java
+++ app/app/src/main/java/com/surverior/android/activity/MainActivity.java
@@ -3,7 +3,11 @@ package com.surverior.android.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -24,7 +28,7 @@ import com.surverior.android.helper.TokenHandler;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 	private static final String TAG = "SurveriorActivity";
 
 	private TextView txtName;
@@ -35,10 +39,17 @@ public class MainActivity extends Activity {
 	private TokenHandler tokendb;
 	private SessionManager session;
 
+	private Toolbar mToolbar;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+		setSupportActionBar(mToolbar);
+		getSupportActionBar().setDisplayShowHomeEnabled(true);
 
 		txtName = (TextView) findViewById(R.id.name);
 		txtEmail = (TextView) findViewById(R.id.email);
@@ -107,6 +118,28 @@ public class MainActivity extends Activity {
 				logoutUser();
 			}
 		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 
 	/**
