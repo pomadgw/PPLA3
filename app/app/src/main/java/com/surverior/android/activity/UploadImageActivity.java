d9a214fe8bc042d8b305ae77bb047ec20f969252
azharfauzandz <azharfauzandz@gmail.com>
2016-04-23 16:38:37 +0700

    Menambah frame di viewprofile, membuat uploadimageActivity

---
+++ app/app/src/main/java/com/surverior/android/activity/UploadImageActivity.java
@@ -0,0 +1,15 @@
+package com.surverior.android.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Azhar Fauzan Dz on 4/22/2016.
+ */
+public class UploadImageActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
