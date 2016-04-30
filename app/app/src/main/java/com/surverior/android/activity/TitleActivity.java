63e109c2046feeac3ef5608105f036a1683ffac5
bmgprakoso <bmgprakoso@gmail.com>
2016-04-30 15:50:12 +0700

    Minor fix di bagian tombol next TitleActivity

--- app/app/src/main/java/com/surverior/android/activity/TitleActivity.java
+++ app/app/src/main/java/com/surverior/android/activity/TitleActivity.java
@@ -36,7 +36,7 @@ public class TitleActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_createsurvey, menu);
+        getMenuInflater().inflate(R.menu.menu_wizard, menu);
         return true;
     }
 
