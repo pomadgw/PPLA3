1559ab0bad065607ac4c421cde59940ee08427a6
Rahadian Yusuf <rahadian.y@gmail.com>
2016-05-01 13:56:21 +0700

    Fix error kirim email verifikasi

--- backend/resources/views/verification.blade.php
+++ backend/resources/views/verification.blade.php
@@ -7,7 +7,7 @@
     <h2 style="text-align: center; color: white;font-family: verdana;"> Please Click Link Below For Confirmation </h2>
     <h3 style="text-align: center; color: #7F6626; font-family: verdana; font-size: 15px;font-style: italic;"> Note: if you can't click the link, please copy and paste this link to your browser </h3>
 
-    <a href="{{url('email-verification/' . $new_user->confirmation_code)}}">{{url('email-verification/' . $new_user->confirmation_code)}}</a>
+    <a href="{{url('email-verification/' . $user->confirmation_code)}}">{{url('email-verification/' . $user->confirmation_code)}}</a>
     <h3 style="text-align: center; color: white;font-family: verdana;padding-top: 40px;"> Welcome to Surverior  </h2>
     <h3 style="text-align: center; color: white;font-family: verdana;padding-top: 20px; font-size: 12px;font-style: underline;"> Find us on </h3>
     <p style="text-align: center;">
